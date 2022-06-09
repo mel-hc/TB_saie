77d2cf0e30fbff1b399c00ddd92cb8f290936902
Melanie H. Chitwood <54188791+mel-hc@users.noreply.github.com>
2022-06-09 15:30:11 -0400

    additional variable descriptions

--- TimeTrendModel/TimeTrend_model_run.R
+++ TimeTrendModel/TimeTrend_model_run.R
@@ -11,14 +11,14 @@ m_state <- read.csv("state_data.csv")
 
 #Model Inputs
 datalist <- list()
-datalist[["N_obs"]]       <-  nrow(m_state)
-datalist[["N_geog"]]      <-  length(unique(m_state$sg_uf)) 
-datalist[["N_time"]]      <-  length(unique(m_state$YEAR)) 
-datalist[["y_notif"]]     <-  as.numeric(m_state$cases)   
-datalist[["y_all_mort"]]  <-  m_state$sim_death
-datalist[["x_year"]]      <-  as.numeric(as.factor(m_state$YEAR))
-datalist[["x_state"]]     <-  as.numeric(as.factor(m_state$sg_uf))
-datalist[["pop_100k"]]    <-  m_state$pop_100k
+datalist[["N_obs"]]       <-  nrow(m_state) # total rows of data (geographies * time points)
+datalist[["N_geog"]]      <-  length(unique(m_state$sg_uf)) # number of geographies
+datalist[["N_time"]]      <-  length(unique(m_state$YEAR)) # number of timepoints
+datalist[["y_notif"]]     <-  as.numeric(m_state$cases)  # vector of TB cases, by location & year 
+datalist[["y_all_mort"]]  <-  m_state$sim_death # vector of TB deaths, by location & year
+datalist[["x_year"]]      <-  as.numeric(as.factor(m_state$YEAR)) # years, indexed from 1
+datalist[["x_state"]]     <-  as.numeric(as.factor(m_state$sg_uf)) # location id, indexed from 1
+datalist[["pop_100k"]]    <-  m_state$pop_100k # vector of population estimates
 datalist[["fhs"]]         <-  as.numeric(m_state$fhs_sc) # FHS coverage (teams per 4000 pop); values > 1 truncated
 datalist[["ln_gdp"]]      <-  as.numeric(m_state$gdp_sc) # gdp per capita in 1000s of $R, 2015
 datalist[["idc"]]         <-  as.numeric(m_state$pct_idc) # percent of total deaths with poorly defined cause
